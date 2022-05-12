8140dcd2c021e29c3b49e16ec76812e1c4d7101d
Esi're <esiri92@gmail.com>
2022-05-12 16:24:11 +0100

    Fix constrain.

--- database/migrations/2022_05_12_160142_create_fixtures_table.php
+++ database/migrations/2022_05_12_160142_create_fixtures_table.php
@@ -11,8 +11,8 @@ class CreateFixturesTable extends Migration
     {
         Schema::create('fixtures', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Team::class, 'home_team_id')->constrained();
-            $table->foreignIdFor(Team::class, 'away_team_id')->constrained();
+            $table->foreignIdFor(Team::class, 'home_team_id')->constrained('teams');
+            $table->foreignIdFor(Team::class, 'away_team_id')->constrained('teams');
             $table->unsignedTinyInteger('home_team_score')->default(0);
             $table->unsignedTinyInteger('away_team_score')->default(0);
             $table->dateTime('date');
